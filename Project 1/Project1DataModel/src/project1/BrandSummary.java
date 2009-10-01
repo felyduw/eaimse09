72b6a98fee6c398a41049b9b7e6a57159d6240b8
pedro.jose.saraiva <pedro.jose.saraiva@591baf3e-a617-11de-b32a-69fa65287987>
2009-10-01 00:35:10 +0000

    New auxiliary classes

--- Project 1/Project1DataModel/src/project1/BrandSummary.java
+++ Project 1/Project1DataModel/src/project1/BrandSummary.java
@@ -1,14 +1,31 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package project1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *
- * @author csimoes
+ * Class to obtain the Summary for each brand.
  */
 public class BrandSummary {
-
+	public String name;
+	public int numberCameras;
+	public List<CameraSummaryDetails> cameras;
+	public List<CameraSummaryDetails> recentCameras; 
+	public List<CameraSummaryDetails> oldestCameras; 
+	public List<CameraSummaryDetails> maxResCameras; 
+	public List<CameraSummaryDetails> minResCameras;
+	
+	/**
+	 * Initialize all variables.
+	 */
+	public BrandSummary() {
+		name = null;
+		numberCameras = 0;
+		cameras = new ArrayList<CameraSummaryDetails>();
+		recentCameras = new ArrayList<CameraSummaryDetails>(); 
+		oldestCameras = new ArrayList<CameraSummaryDetails>(); 
+		maxResCameras = new ArrayList<CameraSummaryDetails>();
+		minResCameras = new ArrayList<CameraSummaryDetails>();
+	}
 }
+
---
+++ Project 1/Project1DataModel/src/project1/CameraSummaryDetails.java
@@ -0,0 +1,12 @@
+package project1;
+
+public class CameraSummaryDetails {
+	public String model;
+	public String date;
+	public String minResolution;
+	public String minResolutionVertical;
+	public String minResolutionHorizontal;
+	public String maxResolution;
+	public String maxResolutionVertical;
+	public String maxResolutionHorizontal;
+}
