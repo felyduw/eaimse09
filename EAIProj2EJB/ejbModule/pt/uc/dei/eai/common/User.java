2b1a0fa51e9e9000cfd31940168bc941804831e2
csimoes.casa <csimoes.casa@591baf3e-a617-11de-b32a-69fa65287987>
2009-10-22 01:42:55 +0000

    getters and setters for User

--- EAIProj2EJB/ejbModule/pt/uc/dei/eai/common/User.java
+++ EAIProj2EJB/ejbModule/pt/uc/dei/eai/common/User.java
@@ -1,9 +1,45 @@
 package pt.uc.dei.eai.common;
 
 public class User {
+	
 	String username;
+	
 	String password;
+	
 	String address;
+	
 	String email;
+	
 	String name;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
