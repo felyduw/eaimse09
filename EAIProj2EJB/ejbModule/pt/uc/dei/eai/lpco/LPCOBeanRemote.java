3fc9828e0067d2e71cfd6f4f272595ba8160c766
csimoes.casa <csimoes.casa@591baf3e-a617-11de-b32a-69fa65287987>
2009-10-23 15:11:09 +0000

    copy code from local to remote

--- EAIProj2EJB/ejbModule/pt/uc/dei/eai/lpco/LPCOBeanRemote.java
+++ EAIProj2EJB/ejbModule/pt/uc/dei/eai/lpco/LPCOBeanRemote.java
@@ -1,7 +1,19 @@
 package pt.uc.dei.eai.lpco;
+import java.util.List;
+
 import javax.ejb.Remote;
+
+import pt.uc.dei.eai.common.Camera;
+import pt.uc.dei.eai.common.Order;
 
 @Remote
 public interface LPCOBeanRemote {
-
+	public boolean doLogin(String username, String password);
+	public boolean doLogout(String username);
+	public boolean addToCart(Integer cameraId);
+	public List<Camera> searchCameras(String searchTerms);
+	public List<String> registerUser(String username, String password, String address, String email);
+	public boolean submitOrder();
+	public List<Order> listAllOrders();
+	public List<Order> listPurchases();
 }
