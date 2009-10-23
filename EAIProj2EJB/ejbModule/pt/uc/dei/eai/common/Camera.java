d6fcb6c1387e04dc299dd5cac180bdeab990faae
csimoes.casa <csimoes.casa@591baf3e-a617-11de-b32a-69fa65287987>
2009-10-23 02:49:25 +0000

    debug added

--- EAIProj2EJB/ejbModule/pt/uc/dei/eai/common/Camera.java
+++ EAIProj2EJB/ejbModule/pt/uc/dei/eai/common/Camera.java
@@ -1,8 +1,31 @@
 package pt.uc.dei.eai.common;
 
 public class Camera {
+	
 	Integer cameraId;
+	
+	String model;
+	
 	Float price;
+	
+	public Integer getCameraId() {
+		return cameraId;
+	}
+	public void setCameraId(Integer cameraId) {
+		this.cameraId = cameraId;
+	}
+	public Float getPrice() {
+		return price;
+	}
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
 	
 	//Characteristics
 	
--- EAIProj2EJB/ejbModule/pt/uc/dei/eai/lpco/LPCOBean.java
+++ EAIProj2EJB/ejbModule/pt/uc/dei/eai/lpco/LPCOBean.java
@@ -37,13 +37,19 @@ public class LPCOBean implements LPCOBeanRemote, LPCOBeanLocal {
 
 	@Override
 	public boolean doLogin(String username, String password) {
-		// TODO Auto-generated method stub
+		// TODO DEBUG - apagar!!!
+		if (username != null && username.equals("carlos")) {
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean doLogout(String username) {
-		// TODO Auto-generated method stub
+		// TODO DEBUG - apagar!!!
+		if (username != null && username.equals("carlos")) {
+			return true;
+		}
 		return false;
 	}
 
@@ -68,7 +74,31 @@ public class LPCOBean implements LPCOBeanRemote, LPCOBeanLocal {
 
 	@Override
 	public List<Camera> searchCameras(String searchTerms) {
-		// TODO Auto-generated method stub
+		// TODO DEBUG - apagar!!!
+		if (searchTerms == null || searchTerms.isEmpty()) {
+			List<Camera> cameras = new ArrayList<Camera>();
+			Camera camera1 = new Camera();
+			camera1.setCameraId(1);
+			camera1.setModel("Panasonic Lumix GF1");
+			camera1.setPrice(10f);
+			cameras.add(camera1);
+			Camera camera2 = new Camera();
+			camera2.setCameraId(2);
+			camera2.setModel("Canon EOS 500D (Digital Rebel T1i / Kiss X3 Digital)");
+			camera2.setPrice(20f);
+			cameras.add(camera2);
+			Camera camera3 = new Camera();
+			camera3.setCameraId(3);
+			camera3.setModel("Leica C-LUX 3");
+			camera3.setPrice(30f);
+			cameras.add(camera3);
+			Camera camera4 = new Camera();
+			camera4.setCameraId(4);
+			camera4.setModel("Leica M8");
+			camera4.setPrice(45f);
+			cameras.add(camera4);
+			return cameras;
+		}
 		return null;
 	}
 
