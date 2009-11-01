8aaa3e5f0cb0dc2b0981bfc608b0bee01957b5f3
mgracaoliveira <mgracaoliveira@591baf3e-a617-11de-b32a-69fa65287987>
2009-11-01 14:23:27 +0000

    bean

--- EAIProj2EJB/ejbModule/pt/uc/dei/eai/lpco/LPCOBean.java
+++ EAIProj2EJB/ejbModule/pt/uc/dei/eai/lpco/LPCOBean.java
@@ -1,16 +1,13 @@
 package pt.uc.dei.eai.lpco;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Random;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateful;
-import javax.xml.namespace.QName;
-import javax.xml.rpc.ServiceException;
-import javax.xml.rpc.ServiceFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 import javax.xml.ws.WebServiceRef;
 
 import org.hibernate.Criteria;
@@ -26,6 +23,10 @@ import pt.uc.dei.eai.common.User;
 import pt.uc.dei.eai.cs.CameraSupplier;
 import pt.uc.dei.eai.cs.CameraSupplierService;
 import pt.uc.dei.eai.data.HibernateUtil;
+import pt.uc.dei.eai.sdep.ShippingDepartment;
+import pt.uc.dei.eai.sdep.ShippingDepartmentService;
+
+import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
 
 /**
  * Session Bean implementation class LPCOBean
@@ -34,7 +35,10 @@ import pt.uc.dei.eai.data.HibernateUtil;
 public class LPCOBean implements LPCOBeanRemote, LPCOBeanLocal {
 	
 	@WebServiceRef(wsdlLocation="http://127.0.0.1:8080/WSCameraSupplier?wsdl")
-    static CameraSupplierService service;
+    static CameraSupplierService CameraService;
+	
+	@WebServiceRef(wsdlLocation="http://127.0.0.1:8080/WSShippingDepartment?wsdl")
+    static ShippingDepartmentService ShippingService;
 
 
 	private User user;
@@ -144,7 +148,7 @@ public class LPCOBean implements LPCOBeanRemote, LPCOBeanLocal {
 		HibernateUtil.commitTransaction();
 		
 		if (result.isEmpty()) { //TODO Camera WS
-			CameraSupplier cs = service.getCameraSupplierPort();
+			CameraSupplier cs = CameraService.getCameraSupplierPort();
 
 			List<pt.uc.dei.eai.cs.Camera> tmp = cs.getCameras(searchTerms);
 
@@ -169,10 +173,10 @@ public class LPCOBean implements LPCOBeanRemote, LPCOBeanLocal {
 		order.setUsername(getUser().getUsername());
 		order.setShippingAddress(getUser().getAddress());
 		order.setPurchaseDate(Calendar.getInstance().getTime());
-
+		Integer identifier;
 		try {
 			Session session = HibernateUtil.beginTransaction();
-			session.save(order);
+			identifier = (Integer) session.save(order);
 			HibernateUtil.commitTransaction();
 		} catch (HibernateException e) {
 			e.printStackTrace();
@@ -185,29 +189,18 @@ public class LPCOBean implements LPCOBeanRemote, LPCOBeanLocal {
 		r.nextInt(100);
 
 		if (r.nextInt(100) < 75) {
-			// Se sim
-			try {
-				// Calling Webservice
-				String wsdlURL = "http://127.0.0.1:8080/WSShippingDepartment?wsdl";
-				String namespace = "http://sdep.eai.dei.uc.pt/";
-				String serviceName = "ShippingDepartmentService";
-				QName serviceQN = new QName(namespace, serviceName);
-
-				ServiceFactory serviceFactory = ServiceFactory.newInstance();
-				// TODO WebService: MUDAR ESTE CODIGO
-				CameraSupplierService service = (CameraSupplierService) serviceFactory
-						.createService(new URL(wsdlURL), serviceQN);
-
-				CameraSupplier cs = service.getCameraSupplierPort();
-				List<pt.uc.dei.eai.cs.Camera> tmp = cs.getCameras("");
-				
-
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			} catch (ServiceException e) {
-				e.printStackTrace();
-			}
+			//TODO Shipping WS
+			ShippingDepartment sd = ShippingService.getShippingDepartmentPort();
 			
+			pt.uc.dei.eai.sdep.Order wsOrder = new pt.uc.dei.eai.sdep.Order();
+			wsOrder.setShippingAddress(order.getShippingAddress());
+			wsOrder.setUsername(order.getUsername());
+			XMLGregorianCalendar calendar = 
+				new XMLGregorianCalendarImpl((GregorianCalendar) GregorianCalendar.getInstance());
+			wsOrder.setPurchaseDate(calendar);
+			wsOrder.setOrderId(identifier);
+			
+			sd.makeOrder(wsOrder);
 			
 		} else {
 			order.setOrderStatus(OrderStatus.NOT_PAID);
