82dd762b0ea67adcd262cbe22a0bdb478e1cd5c2
mgracaoliveira <mgracaoliveira@591baf3e-a617-11de-b32a-69fa65287987>
2009-11-27 17:52:05 +0000

    wsdl urls

---
+++ EAIProj3Web/src/pt/uc/dei/eai/WSDL.java
@@ -0,0 +1,18 @@
+package pt.uc.dei.eai;
+
+public class WSDL {
+	/* User Management */
+	public static String LoginURL = "";
+	public static String RegisterUserURL = "";
+	
+	/* Order Management */
+	public static String SearchCamerasURL = "";
+	public static String GetCameraInfoURL = "";
+	
+	
+	/* Purchase Management */
+	public static String GetPurchaseInfoURL = "";
+	public static String ListPurchasesURL = "";
+	//public static String OrderShippedURL = "";
+	public static String CheckoutURL = "";
+}
