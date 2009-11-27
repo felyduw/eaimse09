00e44c8a5462b73259f7c4c0a89d8e250dd89c4a
mgracaoliveira <mgracaoliveira@591baf3e-a617-11de-b32a-69fa65287987>
2009-11-27 21:40:41 +0000

    urls

--- EAIProj3Web/src/pt/uc/dei/eai/WSDL.java
+++ EAIProj3Web/src/pt/uc/dei/eai/WSDL.java
@@ -5,17 +5,17 @@ public class WSDL {
 	private static String port = "9081";
 	
 	/* User Management */
-	public static String LoginURL = "http://localhost:${HttpDefaultPort}/Login?wsdl";
-	public static String RegisterUserURL = "http://localhost:${HttpDefaultPort}/Register?wsdl";
+	public static String LoginURL = "http://localhost:" + port + "/Login?wsdl";
+	public static String RegisterUserURL = "http://localhost:" + port + "/Register?wsdl";
 	
 	/* Order Management */
-	public static String SearchCamerasURL = "http://localhost:${HttpDefaultPort}/SearchCameras?wsdl";
-	public static String GetCameraInfoURL = "http://localhost:${HttpDefaultPort}/GetCamera?wsdl";
+	public static String SearchCamerasURL = "http://localhost:" + port + "/SearchCameras?wsdl";
+	public static String GetCameraInfoURL = "http://localhost:" + port + "/GetCamera?wsdl";
 	
 	
 	/* Purchase Management */
-	public static String ListPurchasesURL = "http://localhost:${HttpDefaultPort}/GetPurchases?wsdl";
-	public static String GetPurchaseInfoURL = "http://localhost:${HttpDefaultPort}/GetPurchaseInfo?wsdl";
-	//public static String OrderShippedURL = "http://localhost:${HttpDefaultPort}/Shipped?wsdl";
-	public static String CheckoutURL = "http://localhost:${HttpDefaultPort}/Checkout?wsdl";
+	public static String ListPurchasesURL = "http://localhost:" + port + "/GetPurchases?wsdl";
+	public static String GetPurchaseInfoURL = "http://localhost:" + port + "/GetPurchaseInfo?wsdl";
+	//public static String OrderShippedURL = "http://localhost:" + port + "/Shipped?wsdl";
+	public static String CheckoutURL = "http://localhost:" + port + "/Checkout?wsdl";
 }
