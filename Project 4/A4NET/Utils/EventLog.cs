930c2020999369ecfcf4c3c3d3fa7a516bc4aa70
csimoes.casa <csimoes.casa@591baf3e-a617-11de-b32a-69fa65287987>
2009-12-08 06:05:07 +0000

    final

---
+++ Project 4/A4NET/Utils/EventLog.cs
@@ -0,0 +1,18 @@
+﻿
+namespace EAI.A4.Utils
+{
+	public class EventLog
+	{
+		public static void Write2EventLog(string msg)
+		{
+			if (!System.Diagnostics.EventLog.SourceExists("EAIA4NET"))
+			{
+				System.Diagnostics.EventLog.CreateEventSource("EAIA4NET", "Application");
+			}
+			System.Diagnostics.EventLog EventLog1 = new System.Diagnostics.EventLog();
+			EventLog1.Source = "EAIA4NET";
+			EventLog1.WriteEntry(msg);
+		}
+
+	}
+}
---
+++ Project 4/A4NET/Utils/IO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace EAI.A4.Utils
+{
+	public class IO
+	{
+		public static string GetLatestCreatedFile(string directory)
+		{
+			DirectoryInfo di = new DirectoryInfo(directory);
+			FileInfo[] fi = di.GetFiles();
+			string latestCreatedFileName = null;
+			DateTime latestCreatedFileDate = DateTime.MinValue;
+			foreach (FileInfo fiTemp in fi)
+			{
+				if (fiTemp.LastWriteTime > latestCreatedFileDate)
+				{
+					latestCreatedFileDate = fiTemp.LastWriteTime;
+					latestCreatedFileName = fiTemp.Name;
+				}
+			}
+			return latestCreatedFileName;
+		}
+	}
+}
