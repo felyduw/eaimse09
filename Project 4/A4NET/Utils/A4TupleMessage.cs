f3ef3a9f435ab4c52b536d6002e8f1a1fde1c54f
csimoes.casa <csimoes.casa@591baf3e-a617-11de-b32a-69fa65287987>
2009-12-08 17:19:22 +0000

    new files

---
+++ Project 4/A4NET/Utils/A4TupleMessage.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml;
+
+namespace EAI.Common
+{
+	public class A4TupleMessage
+	{
+		public XmlDocument CameraSummaryXmlResult { get; set; }
+
+		public string CameraListBeautifierResult { get; set; }
+
+		public A4TupleMessage()
+		{
+			CameraSummaryXmlResult = null;
+			CameraListBeautifierResult = null;
+		}
+
+		public A4TupleMessage(XmlDocument xmlDoc, string htmlDoc)
+		{
+			CameraSummaryXmlResult = xmlDoc;
+			CameraListBeautifierResult = htmlDoc;
+		}
+	}
+}
