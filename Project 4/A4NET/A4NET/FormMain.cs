826454b22fea5c3ec0610e5951505534f33cfe43
csimoes.casa <csimoes.casa@591baf3e-a617-11de-b32a-69fa65287987>
2009-12-08 06:07:26 +0000

    .

--- Project 4/A4NET/A4NET/FormMain.cs
+++ Project 4/A4NET/A4NET/FormMain.cs
@@ -2,6 +2,8 @@
 using System.Messaging;
 using System.Windows.Forms;
 using EAI.A4.Utils;
+using System.Threading;
+using System.IO;
 
 namespace EAI.A4.UserInterface
 {
@@ -14,14 +16,47 @@ namespace EAI.A4.UserInterface
 		{
 			InitializeComponent();
 			InitializeMessageQueues();
+			InitializeThreads();
 		}
 
 		private void InitializeMessageQueues()
 		{
 			userInterfaceInboxQueue = MessageQueues.CreateOrUseQueue(Properties.Settings.Default.userInterfaceInboxQueue);
 			userInterfaceInboxQueue.Formatter = new XmlMessageFormatter((new System.Type[] { typeof(string) }));
+			userInterfaceInboxQueue.MessageReadPropertyFilter.SetAll();
 			userInterfaceOutboxQueue = MessageQueues.CreateOrUseQueue(Properties.Settings.Default.userInterfaceOutboxQueue);
 			userInterfaceOutboxQueue.Formatter = new XmlMessageFormatter((new System.Type[] { typeof(string) }));
+		}
+
+		private void InitializeThreads()
+		{
+			ThreadStart jobReceiveMessages = new ThreadStart(ReceiveMessages);
+			Thread threadReceiveMessages = new Thread(jobReceiveMessages);
+			threadReceiveMessages.Start();
+		}
+
+		private void ReceiveMessages()
+		{
+			while (true)
+			{
+				// receives message
+				System.Messaging.Message incomingMsg = userInterfaceInboxQueue.Receive();
+				string msg = (string)incomingMsg.Body;
+				// write to the file
+				string filename = "Final-" + incomingMsg.CorrelationId.Replace(@"\", "_") + ".html";
+				File.WriteAllText(filename, msg);
+				// open dialog to inform
+				SetTextCallback d = new SetTextCallback(CallMessageBox);
+				this.Invoke(d, new object[] { filename });
+			}
+		}
+
+		delegate void SetTextCallback(string filename);
+
+		private void CallMessageBox(string filename)
+		{
+			MessageBox.Show(this, "new file: " + filename, "EAI A4 - UserInterface", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			toolStripStatusLabelResults.Text = String.Empty;
 		}
 
 		#region Handlers
